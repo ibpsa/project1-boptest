5609f9b1815ea3185eb218e1d822031b4f2f032e
Huang <huang875@pnnl.gov>
2018-07-27 10:30:53 -0700

    Add the overwritten block to the test case

---
+++ testcase/From_senhuang/Server.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+This module starts a socket server which is keeping listening to the specified port for incoming message;
+Once revived any message, it will send the same content back to the corresponding client.
+
+"""
+
+import socket
+
+class socket_server:
+
+  def __init__(self):
+      self.sock=socket.socket()
+      port=8888
+      host='127.0.0.1'
+      self.sock.bind((host,port))
+      self.sock.listen(10)
+
+server = socket_server()
+### data received from Modelica
+
+while True:
+    conn,addr=server.sock.accept()
+#    print('Got a connection from {}.'.format(addr))
+    data = conn.recv(1024)
+    print data
+
+### inputs for Modelica models
+    inputs=''
+    datas=data.split(',')
+### Getting rid of the variable names from the incoming message
+    for i in range((len(datas)-1)/2):
+         inputs=inputs+str(datas[i*2+1])+','
+#    print inputs
+    conn.send(inputs[:-1])
