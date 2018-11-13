f123a14f1189c5c85912011cf91a6ac5288c862d
UbuntuWS <dhblum@lbl.gov>
2018-11-13 15:17:12 -0800

    Update and clean to use fmu for parsing.

--- examples/simpleODE/parse_signal_exchange.py
+++ examples/simpleODE/parse_signal_exchange.py
@@ -1,74 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-This script implements a test of parsing the signal exchange blocks.
+Implements the parsing and code generation for signal exchange blocks.
 
-1) Use Modelica-Json to parse Modelica code
-2) Load json and find signal exchange block class instances
+The steps are:
+1) Compile Modelica code into fmu
+2) Use signal exchange block id parameters to find block instance paths
 3) Write Modelica wrapper around instantiated model
-4) Export as FMU
+4) Export as wrapper FMU
 
 """
 
-import subprocess
-import json
-import pprint
 from pyfmi import load_fmu
 from pymodelica import compile_fmu
-
-pp = pprint.PrettyPrinter(indent=2)
-
-def parse_json(mo_filepath):
-    '''Use Modelica-Json to parse the Modelica file.
-    
-    Parameters
-    ----------
-    mo_filepath : str
-        File path of the Modelica file to parse
-    
-    Returns
-    -------
-    mo_json : dict
-        Dictionary representing the output of Modelica-Json
-        
-    '''
-
-    # Run modelica-json
-    command = 'node /home/dhb-lx/git/antlr/modelica-json/app.js -f author/TestOverWrite_Unpacked/ -o json-simplified -m modelica'
-    subprocess.check_output(command.split(' '))
-    # Load json output
-    with open('author/TestOverWrite_Unpacked/simplified/TestOverWrite_Unpacked.OriginalModel.json') as f:
-        mo_json = json.load(f)[0]
-    
-    return mo_json
-    
-def get_classes_json(mo_json):
-    '''Find the signal exchange block class instances in a modelica-json.
-    
-    Parameters
-    ----------
-    mo_json : dict
-        Dictionary representing the output of Modelica-Json
-        
-    Returns
-    -------
-    classes : dict
-        Dictionary of overwrite and read block class lists.
-        {'Overwrite': [str], 'Read': [str]}
-        
-    '''
-    
-    # Initialize dictionary
-    classes = {'Overwrite':[], 'Read':[]}
-    # Find classes of 'Overwrite' or 'Read'
-    for model in mo_json['public']['models']:
-        if model['className'] == u'Overwrite':
-            classes['Overwrite'].append(model['name'])
-        elif model['className'] == u'Read':
-            classes['Read'].append(model['name'])
-            
-    return classes
-    
-def get_classes_xml(model_path, mo_path):
+import os
+  
+def get_instances(model_path, mo_path):
     '''Find the signal exchange block class instances using fmu xml.
     
     Parameters
@@ -80,8 +26,8 @@ def get_classes_xml(model_path, mo_path):
         
     Returns
     -------
-    classes : dict
-        Dictionary of overwrite and read block class lists.
+    instances : dict
+        Dictionary of overwrite and read block class instance lists.
         {'Overwrite': [str], 'Read': [str]}
         
     '''
@@ -95,8 +41,8 @@ def get_classes_xml(model_path, mo_path):
     # Get all parameters
     par_names = fmu.get_model_variables(causality = 0).keys()
     # Initialize dictionary
-    classes = {'Overwrite':[], 'Read':[]}
-    # Find classes of 'Overwrite' or 'Read'
+    instances = {'Overwrite':[], 'Read':[]}
+    # Find instances of 'Overwrite' or 'Read'
     for par in par_names:
         # Overwrite
         if 'is_overwrite' in par:
@@ -106,14 +52,17 @@ def get_classes_xml(model_path, mo_path):
             label = 'Read'
         else:
             continue
-        # Get class instance name
+        # Get instance name
         instance = '.'.join(par.split('.')[:-1])
         # Save instance name with label
-        classes[label].append(instance)
+        instances[label].append(instance)
+    # Clean up
+    os.remove(fmu_path)
+    os.remove(fmu_path.replace('.fmu', '_log.txt'))
             
-    return classes
+    return instances
     
-def write_wrapper(model_path, mo_path, classes):
+def write_wrapper(model_path, mo_path, instances):
     '''Write the wrapper modelica model and export as fmu
     
     Parameters
@@ -122,8 +71,8 @@ def write_wrapper(model_path, mo_path, classes):
         Path to orginal modelica model
     mo_path : str
         Path to orginal modelica file
-    classes : dict
-        Dictionary of overwrite and read block class lists.
+    instances : dict
+        Dictionary of overwrite and read block class instance lists.
         {'Overwrite': [str], 'Read': [str]}
 
     Returns
@@ -145,7 +94,7 @@ def write_wrapper(model_path, mo_path, classes):
         f.write('\t// Input overwrite\n')
         input_signals = dict()
         input_activate = dict()
-        for block in classes['Overwrite']:
+        for block in instances['Overwrite']:
             # Replace dots with lines
             var_name = block.replace('.', '_')
             # Add to signal input list
@@ -158,7 +107,7 @@ def write_wrapper(model_path, mo_path, classes):
             f.write('\tModelica.Blocks.Interfaces.BooleanInput {0} "Activation for overwrite block {1}";\n'.format(input_activate[block], block))
         # Add outputs for every read block
         f.write('\t// Out read\n')
-        for block in classes['Read']:
+        for block in instances['Read']:
             # Replace dots with lines
             var_name = block.replace('.', '_')
             # Instantiate input signal
@@ -167,10 +116,10 @@ def write_wrapper(model_path, mo_path, classes):
         f.write('\t// Original model\n')
         f.write('\t{0} mod(\n'.format(model_path))
         # Connect inputs to original model overwrite and activate signals
-        for i,block in enumerate(classes['Overwrite']):
+        for i,block in enumerate(instances['Overwrite']):
             f.write('\t\t{1}(uExt(y={0})),\n'.format(input_signals[block], block))
             f.write('\t\t{1}(activate(y={0}))'.format(input_activate[block], block))   
-            if i == len(classes['Overwrite'])-1:
+            if i == len(instances['Overwrite'])-1:
                 f.write(') "Original model with overwrites";\n')
             else:
                 f.write(',\n')
@@ -187,11 +136,12 @@ if __name__ == '__main__':
     model_path = 'TestOverWrite.OriginalModelStacked'
     mo_path = 'author/TestOverWrite.mo'
     # Get classes
-    classes = get_classes_xml(model_path, mo_path)
+    instances = get_instances(model_path, mo_path)
     # Write wrapper
-    write_wrapper(model_path, mo_path, classes)
-    # Print
-    pp.pprint(classes)
+    fmu_path, wrapped_path = write_wrapper(model_path, mo_path, instances)
+    # Print information
+    print('Exported FMU path is: {0}'.format(fmu_path))
+    print('Written wrapper model path is: {0}'.format(wrapped_path))
 
 
     
