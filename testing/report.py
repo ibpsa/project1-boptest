ff609bd0d225d6eae2c2a2b4ba3b6123414adbdc
David Blum <dhblum@lbl.gov>
2019-02-25 13:01:57 -0500

    Add exit code to unittest reporting.

--- testing/report.py
+++ testing/report.py
@@ -36,47 +36,49 @@ def record(message, display=True, write=True, initial=False):
         with open(report_file, access) as logf:
             logf.write('\n'+message)
 
-record('\n==============\nTESTING REPORT\n==============\n', initial=True)
-# Get all test case logs
-testing_dir = os.path.join(utilities.get_root_path(),'testing')
-logs = []
-for f in os.listdir(testing_dir):
-    if f.endswith('.log'):
-        logs.append(os.path.join(testing_dir, f))
-
-# Get total number of tests run
-cases = 0
-passed = 0
-record('The following test modules were run:')
-for log in logs:
-    with open(log, 'r') as f:
-        d = json.load(f)
-    cases = cases + d['NCases']
-    passed = passed + d['NPassed']
-    record(d['TestFile'])
-
-# Report total number run
-record('The total number of individual tests run was {0}.'.format(cases))
-# Report if all passed
-if cases == passed:
-    record('\n-------------\nAll tests OK.\n-------------\n\nOutput saved to {0}.'.format(report_file))
-else:
-    record('\n-------------\nNot all tests passed.  See {0} for more detailed report.\n-------------\n'.format(report_file))
+if __name__ == '__main__':
+    exit_code = 0
+    record('\n==============\nTESTING REPORT\n==============\n', initial=True)
+    # Get all test case logs
+    testing_dir = os.path.join(utilities.get_root_path(),'testing')
+    logs = []
+    for f in os.listdir(testing_dir):
+        if f.endswith('.log'):
+            logs.append(os.path.join(testing_dir, f))
+    # Get total number of tests run
+    cases = 0
+    passed = 0
+    record('The following test modules were run:')
     for log in logs:
         with open(log, 'r') as f:
             d = json.load(f)
-        record('\nFrom {0}\n========================='.format(d['TestFile']))
-        record('Of {0} tests run...'.format(d['NCases']))
-        record('Passed: {0}'.format(d['NPassed']))
-        record('Failures: {0}'.format(d['NFailures']))
-        record('Errors: {0}'.format(d['NErrors']))
-        record('\nFailure Messages\n----------------')
-        for failure in d['Failures'].keys():
-            record('Failure {0}:\n{1}'.format(failure, d['Failures'][failure]))
-        record('\nError Messages\n----------------')
-        for error in d['Errors'].keys():
-            record('Error {0}:\n{1}'.format(error, d['Errors'][error]))
-            
-# Clean up test logs
-for log in logs:
-    os.remove(log)
+        cases = cases + d['NCases']
+        passed = passed + d['NPassed']
+        record(d['TestFile'])
+    # Report total number run
+    record('The total number of individual tests run was {0}.'.format(cases))
+    # Report if all passed
+    if cases == passed:
+        record('\n-------------\nAll tests OK.\n-------------\n\nOutput saved to {0}.'.format(report_file))
+    else:
+        exit_code = 1
+        record('\n-------------\nNot all tests passed.  See {0} for more detailed report.\n-------------\n'.format(report_file))
+        for log in logs:
+            with open(log, 'r') as f:
+                d = json.load(f)
+            record('\nFrom {0}\n========================='.format(d['TestFile']))
+            record('Of {0} tests run...'.format(d['NCases']))
+            record('Passed: {0}'.format(d['NPassed']))
+            record('Failures: {0}'.format(d['NFailures']))
+            record('Errors: {0}'.format(d['NErrors']))
+            record('\nFailure Messages\n----------------')
+            for failure in d['Failures'].keys():
+                record('Failure {0}:\n{1}'.format(failure, d['Failures'][failure]))
+            record('\nError Messages\n----------------')
+            for error in d['Errors'].keys():
+                record('Error {0}:\n{1}'.format(error, d['Errors'][error]))
+    # Clean up test logs
+    for log in logs:
+        os.remove(log)
+    # Return exit code
+    exit(exit_code)
