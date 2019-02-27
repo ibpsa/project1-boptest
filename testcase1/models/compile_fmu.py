78d7ac61108265aed47fd392f854a2bbe5217619
Javier Arroyo <javier.arroyo@kuleuven.be>
2019-02-27 10:10:17 -0800

    deleted configure_JModelica from compile_fmu

--- testcase1/models/compile_fmu.py
+++ testcase1/models/compile_fmu.py
@@ -8,29 +8,7 @@ The following libraries must be on the MODELICAPATH:
 - Modelica IBPSA
 
 """
-import os
-import sys
-
-# Configure environment for JModelica
-GBT_dir = 'C:\Users\u0110910\Box Sync\work_folder\GBT' 
-JM_dir  = 'C:\JModelica.org-2.2'
-
-# Import the python library of the Grey-Box Toolbox 
-GBTpy_dir = os.path.join(GBT_dir, os.path.join('greybox','python'))
-sys.path.append(GBTpy_dir)
-
-import configure_jmodelica
-configure_jmodelica.main()  
-
-# Set a 'ModelicaPath' environmental variable with the path to 
-# Modelica and IBPSA libraries
-Modelica_dir = os.path.join(JM_dir, os.path.join('install',os.path.join('ThirdParty','MSL')))
-ibpsa_dir = "C:\\Users\\u0110910\\Box Sync\\work_folder\\modelicalib\\modelica-ibpsa"
-os.environ["MODELICAPATH"] = Modelica_dir+';'+ibpsa_dir  
-
-sys.path.append('C:\\Users\\u0110910\\Box Sync\\work_folder\\BOPTEST')
-
-from _parser import parser
+from parser import parser
 
 # DEFINE MODEL
 # ------------
