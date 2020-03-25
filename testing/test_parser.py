264e00b0d2d75c96d7a29b7cb19610f155cb0559
Javier Arroyo <javier.arroyo@kuleuven.be>
2020-03-25 11:07:15 +0100

    Implement multi-zone unit-test for parser

--- testing/test_parser.py
+++ testing/test_parser.py
@@ -189,6 +189,28 @@ class ParseInstances(unittest.TestCase):
         # Delete leftover files
         utilities.clean_up(testing_root_dir)
 
+class ParseInstancesMultiZone(unittest.TestCase, utilities.partialTimeseries):
+    '''Tests the parse_instances method of parser for multi-zone models. 
+    
+    '''
+    
+    def setUp(self):
+        '''Setup for each test.
+        
+        '''
+        
+        # Model path and file specifically for this case
+        self.model_path = 'TwoZones'
+        self.mo_path = os.path.join(testing_root_dir,'parsing', 'TwoZones.mo')
+
+    def test_export(self):
+        # Parse and export fmu to working directory
+        self.fmu_path, self.kpi_path = parser.export_fmu(self.model_path, [self.mo_path])
+        # Test
+        ref_kpis_json_path = os.path.join(testing_root_dir, 'references', 'parser', 'kpis_MultiZone.json')
+        passed = _compare_kpis_json(self.fmu_path, ref_kpis_json_path)
+        self.assertTrue(passed, 'Did not pass kpi json check')
+
 class WriteWrapper(unittest.TestCase):
     '''Tests the write_wrapper method of parser.
     
