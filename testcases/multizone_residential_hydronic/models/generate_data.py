76c3e10520f62a491c4350a137fa50ee1929b952
Javier Arroyo <javier.arroyo@kuleuven.be>
2020-04-23 16:48:35 +0200

    Generate weather data with 5 minutes interval

--- testcases/multizone_residential_hydronic/models/generate_data.py
+++ testcases/multizone_residential_hydronic/models/generate_data.py
@@ -10,6 +10,6 @@ import os
 file_dir = os.path.dirname(os.path.realpath(__file__))
 resources_dir = os.path.join(file_dir, 'Resources')
 # Create data generator object with time interval to 15 minutes
-gen = Data_Generator(resources_dir, period=900)
+gen = Data_Generator(resources_dir, period=300)
 # Generate weather data from .mos in Resources folder with default values
 gen.generate_weather()
