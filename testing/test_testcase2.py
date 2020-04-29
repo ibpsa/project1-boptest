29d19fa1a655519f728c660eb0cb6e741f3ae1a1
David Blum <dhblum@lbl.gov>
2020-04-29 09:53:39 -0700

    Fix for unittests

--- testing/test_testcase2.py
+++ testing/test_testcase2.py
@@ -137,6 +137,7 @@ class API(unittest.TestCase, utilities.partialTestAPI):
         self.name = 'testcase2'
         self.url = 'http://127.0.0.1:5000'
         self.name_ref = 'wrapped'
+        self.step_ref = 3600.0
 
 if __name__ == '__main__':
     utilities.run_tests(os.path.basename(__file__))
