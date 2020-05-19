10ee6466772e20bfcbd9c1c5919cdf8621b8bf67
David Blum <dhblum@lbl.gov>
2020-05-19 10:17:03 -0700

    Correct module paths for custom kpi

--- testing/test_testcase2.py
+++ testing/test_testcase2.py
@@ -30,7 +30,8 @@ class ExampleSupervisoryPython(unittest.TestCase, utilities.partialChecks):
         '''
         
         # Run test
-        kpi,res,customizedkpis_result = szvav_sup.run()
+        custom_kpi_config_path = os.path.join(utilities.get_root_path(), 'examples', 'python', 'custom_kpi', 'custom_kpis_example.config')
+        kpi,res,customizedkpis_result = szvav_sup.run(customized_kpi_config=custom_kpi_config_path)
         # Check kpis
         df = pd.DataFrame.from_dict(kpi, orient='index', columns=['value'])
         df.index.name = 'keys'
