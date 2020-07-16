4a4e5811e36cd4cf956c1dc052d5366d03872635
Javier Arroyo <javier.arroyo@kuleuven.be>
2020-07-16 19:14:09 +0200

    Add set_library_versions.sh

---
+++ testcases/bestest_hydronic_heat_pump/models/set_library_versions.sh
@@ -0,0 +1,15 @@
+# This file is used by unit testing to specify library versions.
+# Commands here should maintain alignment with the environment generated in 
+# testing/Dockerfile.
+
+# Set modelica library commits here
+# =================================
+IBPSA_COMMIT=master
+BUILDINGS_COMMIT=master
+IDEAS_COMMIT=issue1144_hydronictestWithHP
+# =================================
+
+# Executes corresponding git checkout
+cd $MODELICAPATH/IBPSA && git checkout $IBPSA_COMMIT
+cd $MODELICAPATH/Buildings && git checkout $BUILDINGS_COMMIT
+cd $MODELICAPATH/IDEAS && git checkout $IDEAS_COMMIT
