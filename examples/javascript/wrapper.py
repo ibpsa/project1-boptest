d381c56139401377d5753d5e2ac2dcd12efcb793
David Blum <dhblum@lbl.gov>
2021-01-10 09:41:40 -0800

    Restructure javascript htmls to run unit tests faster and reach correct result

--- examples/javascript/wrapper.py
+++ examples/javascript/wrapper.py
@@ -14,7 +14,7 @@ browser = Browser('firefox', headless=True)
 browser.visit('file://' + os.path.realpath(sys.argv[1]))
 
 
-if sys.argv[1].find('szvav')!=-1:
+if sys.argv[1].find('testcase2')!=-1:
     testcase = 2
 else:
     testcase = 1
@@ -24,33 +24,37 @@ sim_tim=0
 while sim_tim < 3600*48:
 
   ########## record the data for simulation output ##########
-  
+
   elements = browser.find_by_id("output")
-  
+
   ########## extract the simulation time ##########
 
   div = elements[0]
   if div.value !='':
-     result= eval(div.value) 
+     result= eval(div.value)
      sim_tim = result['time']
 
 ########## extract the final simulation output ##########
 
-results = browser.find_by_id("result")
+try_results = True
+while try_results:
+    try:
+        results = browser.find_by_id("result")
 
-div = results[0]
+        div = results[0]
 
-if div.value !='':
-     result= eval(div.value)
+        if div.value !='':
+             result= eval(div.value)
 
+        tab=pd.DataFrame(result['u'])
+        try_results = False
+    except:
+        try_results = True
 
-
-tab=pd.DataFrame(result['u']) 
-
-tab2=pd.DataFrame(result['y']) 
+tab2=pd.DataFrame(result['y'])
 
 tab=tab.merge(tab2, on=['time'], how='inner')
-
+tab = tab.set_index('time')
 tab.to_csv('/home/result_testcase{}.csv'.format(testcase))
 
 
@@ -62,10 +66,8 @@ if div.value !='':
      kpis= eval(div.value)
 
 print kpis
-
-tab=pd.DataFrame(kpis, index=[0]) 
-
-
+tab = pd.DataFrame.from_dict(kpis, orient='index', columns=['value'])
+tab.index.name = 'keys'
 
 tab.to_csv('/home/kpi_testcase{}.csv'.format(testcase))
 
