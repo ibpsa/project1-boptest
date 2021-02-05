8785bb22d0882e46f8d16085db27c10d86aa47f5
Javier Arroyo <javier.arroyo@kuleuven.be>
2021-02-05 18:16:01 +0100

    Find days for bestest_hydronic

---
+++ testcases/bestest_hydronic/models/find_days.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 5, 2021
+
+@author: Javier Arroyo
+
+Finds the peak and typical heat days for the bestest_hydronic
+case. This case needs to be deployed. 
+
+'''
+
+from data.find_days import find_days
+import json
+
+days = find_days(heat='reaQHea_y', cool=None, data='simulate')
+
+with open('days.json', 'w') as f:
+    json.dump(days, f)
