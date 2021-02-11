78056613cfdda5b15e0204c039d2fd98b6a23c92
Javier Arroyo <javier.arroyo@kuleuven.be>
2021-02-11 15:17:36 +0100

    Use thermal load (instead of electric power) to find days in bestest_hydronic_heat_pump

--- testcases/bestest_hydronic_heat_pump/models/find_days.py
+++ testcases/bestest_hydronic_heat_pump/models/find_days.py
@@ -11,7 +11,8 @@ case. This case needs to be deployed.
 from data.find_days import find_days
 import json
 
-days = find_days(heat='reaPHeaPum_y', cool=None, data='simulate')
+days = find_days(heat='reaQHeaPumCon.y', cool=None, 
+                 data='simulate', img_name='boptest_bestest_hydronic_heat_pump')
 
 with open('days.json', 'w') as f:
     json.dump(days, f)
