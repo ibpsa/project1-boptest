aeb9ecb86fb471b492e34a3199835d774cdbeb3d
David Blum <dhblum@lbl.gov>
2021-03-03 08:28:52 -0800

    Fix internal load boundary condition data

--- testcases/multizone_office_simple_air/models/generate_internal_setpoints_data.py
+++ testcases/multizone_office_simple_air/models/generate_internal_setpoints_data.py
@@ -20,7 +20,13 @@ mapper_zones = {'nor':'North',
                 'wes':'West',
                 'cor':'Core'}
 
-df = pd.read_csv('Resources/internal_setpoints_occupancy_sim.csv',index_col='Time')
+area = {'North':207.58, # m^2
+        'South':207.58,
+        'East':131.416,
+        'West':131.416,
+        'Core':984.672}
+
+df = pd.read_csv('internal_setpoints_occupancy_sim.csv',index_col='Time')
 
 # Internal Loads and Setpoints
 mapper = {}
@@ -30,20 +36,17 @@ for key in df.columns:
             for zone in mapper_zones.keys():
                 if zone in key.lower():
                     mapper[key] = mapper_names[name]+'[{0}]'.format(mapper_zones[zone])
+                    if 'qGai_flow' in key:
+                        df[key] = df[key].values*area[mapper_zones[zone]]
 df = df.rename(columns=mapper)
 df.index.name = 'time'
 
 # Occupancy
 lat_per = 80 # W/per
-area = {'North':207.58, # m^2
-        'South':207.58,
-        'East':131.416,
-        'West':131.416,
-        'Core':984.672}
 for key in df.columns:
     if 'InternalGainsLat' in key:
         zone = re.findall(r'\[([^]]*)\]', key)[0]
-        df['Occupancy[{0}]'.format(zone)] = (df['InternalGainsLat[{0}]'.format(zone)].values*area[zone]/lat_per).astype(int)
+        df['Occupancy[{0}]'.format(zone)] = (df['InternalGainsLat[{0}]'.format(zone)].values/lat_per).astype(int)
 
 # CO2 Limits
 for zone in mapper_zones:
