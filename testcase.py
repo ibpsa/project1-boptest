d1ca6cf433b77a057b5f3ada165477fec0dbb19c
Javier Arroyo <javier.arroyo@kuleuven.be>
2021-03-19 13:00:36 +0100

    Reorder __init__ and resolve indentation issues.

--- testcase.py
+++ testcase.py
@@ -48,20 +48,14 @@ class TestCase(object):
         # Get input and output meta-data
         self.inputs_metadata = self._get_var_metadata(self.fmu, self.input_names, inputs=True)
         self.outputs_metadata = self._get_var_metadata(self.fmu, self.output_names)
-        # Initialize simulation data arrays
-        self.__initilize_data()
-        # Instantiate a KPI calculator for the test case
-        self.cal = KPI_Calculator(testcase=self)
         # Set default communication step
         self.set_step(con['step'])
         # Set default forecast parameters
         self.set_forecast_parameters(con['horizon'], con['interval'])
-        # Set default price scenario
-        self.set_scenario(con['scenario'])
         # Set default fmu simulation options
         self.options = self.fmu.simulate_options()
         self.options['CVode_options']['rtol'] = 1e-6
-	self.options['CVode_options']['store_event_points'] = False 
+        self.options['CVode_options']['store_event_points'] = False 
         # Assign initial testing time
         self.initial_time = 0
         # Set initial fmu simulation start
@@ -69,6 +63,12 @@ class TestCase(object):
         self.initialize_fmu = True
         self.options['initialize'] = self.initialize_fmu
         self.elapsed_control_time = []
+        # Initialize simulation data arrays
+        self.__initilize_data()
+        # Instantiate a KPI calculator for the test case
+        self.cal = KPI_Calculator(testcase=self)
+        # Set default price scenario
+        self.set_scenario(con['scenario'])
 
     def __initilize_data(self):
         '''Initializes objects for simulation data storage.
@@ -97,8 +97,9 @@ class TestCase(object):
         for key in self.input_names:
             self.u[key] = np.array([])
         self.u_store = copy.deepcopy(self.u)
-	# Results filtering for pyfmi
-        self.options['filter'] = self.output_names + self.input_names                
+        # Results filtering for pyfmi
+        self.options['filter'] = self.output_names + self.input_names
+                        
     def __simulation(self,start_time,end_time,input_object=None):
         '''Simulates the FMU using the pyfmi fmu.simulate function.
 
@@ -125,7 +126,7 @@ class TestCase(object):
         self.options['ncp'] = int((end_time-start_time)/30)
         # Simulate fmu
         try:
-             res = self.fmu.simulate(start_time = start_time,
+            res = self.fmu.simulate(start_time = start_time,
                                      final_time = end_time,
                                      options=self.options,
                                      input=input_object)
