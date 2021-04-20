b27ebc6dbcd960955546f9551f0820f3f2219407
Javier Arroyo <javier.arroyo@kuleuven.be>
2021-04-20 17:09:36 +0200

    Add script to compare references for bestest_air case

---
+++ testcases/bestest_air/models/compare_references.py
@@ -0,0 +1,20 @@
+'''
+Created on Apr 16, 2021
+
+@author: Javier Arroyo
+
+'''
+
+from testing.utilities import compare_references
+
+compare_references(vars_timeseries = ['fcu_reaPHea_y',
+                                      'fcu_reaPFan_y',
+                                      'fcu_reaPCoo_y',
+                                      'fcu_reaFloSup_y',
+                                      'fcu_reaTSup_y',
+                                      'con_reaTSetCoo_y',
+                                      'con_reaTSetHea_y',
+                                      'zon_reaTRooAir_y',
+                                      'LowerSetp[1]'],
+                   refs_old = 'bestest_air_old',
+                   refs_new = 'bestest_air')
