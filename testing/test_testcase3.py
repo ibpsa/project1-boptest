d2e5aeedb948d8dbd1a9cfdc2418a5bcc743a17d
Kyle Benne <kyle.benne@nrel.gov>
2021-06-29 10:27:00 -0500

    Enable testcase3 and example

--- testing/test_testcase3.py
+++ testing/test_testcase3.py
@@ -5,11 +5,13 @@ already be deployed.
 
 """
 
+import requests
 import unittest
 import pandas as pd
 import os
 import utilities
 from examples.python import testcase3
+from boptest_client import BoptestClient
 
 class ExampleProportionalPython(unittest.TestCase, utilities.partialChecks):
     '''Tests the example test of proportional feedback controller with
@@ -48,15 +50,25 @@ class API(unittest.TestCase, utilities.partialTestAPI):
 
     '''
 
+    @classmethod
+    def setUpClass(cls):
+        cls.name = 'testcase3'
+        cls.url = 'http://127.0.0.1:80'
+        client = BoptestClient(cls.url)
+        cls.testid = client.submit('testcases/{0}/models/wrapped.fmu'.format(cls.name))
+
     def setUp(self):
         '''Setup for testcase.
 
         '''
-
-        self.name = 'testcase3'
-        self.url = 'http://127.0.0.1:5000'
+        self.name = API.name
+        self.url = API.url
         self.step_ref = 60.0
+        self.testid = API.testid
         self.test_time_period = 'test_day'
+
+    def tearDown(self):
+        requests.put('{0}/stop/{1}'.format(self.url, self.testid))
 
 if __name__ == '__main__':
     utilities.run_tests(os.path.basename(__file__))
