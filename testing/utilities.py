13586030d0881e9173491387d7ba13ebb9ae03b1
David Blum <dhblum@lbl.gov>
2021-07-07 11:12:59 -0700

    Correct typos discovered in inputs

--- testing/utilities.py
+++ testing/utilities.py
@@ -567,10 +567,10 @@ class partialTestAPI(partialChecks):
             u = {'oveTSetSup_activate':0, 'oveTSetSup_u':273.15+60,
                  'ovePum_activate':0, 'ovePum_u':1}
         elif self.name == 'bestest_hydronic_heat_pump':
-            u = {'oveTSetHea_activate':0, 'oveTSetHea_u':273.15+22}
+            u = {'oveTSet_activate':0, 'oveTSet_u':273.15+22}
         elif self.name == 'multizone_residential_hydronic':
             u = {'conHeaRo1_oveTSetHea_activate':0, 'conHeaRo1_oveTSetHea_u':273.15+22,
-                 'oveEmiPum_oveActHea_activate':0, 'oveEmiPum_oveActHea_u':1}
+                 'oveEmiPum_activate':0, 'oveEmiPum_u':1}
         requests.put('{0}/initialize'.format(self.url), data={'start_time':0, 'warmup_period':0})
         requests.put('{0}/step'.format(self.url), data={'step':self.step_ref})
         y = requests.post('{0}/advance'.format(self.url), data=u).json()
