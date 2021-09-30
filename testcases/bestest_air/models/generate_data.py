fdef7edf510b6cb30791fe9e5cabeac360cddb5d
David Blum <dhblum@lbl.gov>
2021-09-30 14:41:52 -0700

    Update bestest_air and multizone_residential weather data

--- testcases/bestest_air/models/generate_data.py
+++ testcases/bestest_air/models/generate_data.py
@@ -12,8 +12,19 @@ con.TSetHea.y[1]
 
 '''
 
+from data.data_generator import Data_Generator
 import pandas as pd
 import numpy as np
+import os
+
+# Set the location of the Resource directory relative to this file location
+file_dir = os.path.dirname(os.path.realpath(__file__))
+resources_dir = os.path.join(file_dir, 'Resources')
+# Create data generator object with time interval to 15 minutes
+gen = Data_Generator(resources_dir, period=900)
+
+# Generate weather data from .mos in Resources folder with default values
+gen.generate_weather()
 
 # Remove duplicate states
 df = pd.read_csv('Resources/TestCase_Ideal.csv')
