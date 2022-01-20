9b75ed9c2e41079325bb18b5dec911d5434a1ddc
David Blum <dhblum@lbl.gov>
2022-01-20 08:19:28 -0800

    Take max only once

--- kpis/kpi_calculator.py
+++ kpis/kpi_calculator.py
@@ -391,10 +391,10 @@ class KPI_Calculator(object):
             df_pow_data_all.index = pd.TimedeltaIndex(df_pow_data_all.index, unit='s')
             df_pow_data_all = df_pow_data_all.resample('15T').mean()/self.case._get_area()/1000.
             df_pow_data_all['total_demand'] = df_pow_data_all.sum(axis=1)
-            peak = df_pow_data_all['total_demand'].max()
+            i = df_pow_data_all['total_demand'].idxmax()
+            peak = df_pow_data_all.loc[i,'total_demand']
             self.pele_tot = peak
             # Find contributions to peak by each signal
-            i = df_pow_data_all['total_demand'].idxmax()
             for signal in self.case.kpi_json[source]:
                 self.pele_dict[signal] = df_pow_data_all.loc[i,signal]
         # Assign to case
