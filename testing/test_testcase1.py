998af6c103b176d333720207230672377304eba5
rlutes <robert.lutes@pnnl.gov>
2022-06-16 17:14:29 -0700

    Initialize step for utilities unit tests.

--- testing/test_testcase1.py
+++ testing/test_testcase1.py
@@ -341,6 +341,7 @@ class API(unittest.TestCase, utilities.partialTestAPI):
         self.url = 'http://127.0.0.1:5000'
         self.step_ref = 60.0
         self.test_time_period = 'test_day'
+        requests.put('{0}/step'.format(self.url), data={'step': self.step_ref})
 
 if __name__ == '__main__':
     utilities.run_tests(os.path.basename(__file__))
