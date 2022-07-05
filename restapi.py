1e93eb94ff86c40971f0ac86a25e002dc500f419
David Blum <dhblum@lbl.gov>
2022-07-05 13:17:51 -0700

    Remove debug=True

--- restapi.py
+++ restapi.py
@@ -254,4 +254,4 @@ api.add_resource(Version, '/version')
 # --------------------------------------
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
