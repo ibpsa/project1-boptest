456244db76bd863bf80ed076cb36375764a94a4a
Tim Coleman <tjc.coleman@gmail.com>
2022-07-14 12:52:06 -0600

    add in the script to push images to docker hub

---
+++ .github/workflows/publish_to_docker.sh
@@ -0,0 +1,29 @@
+# WEB_REGISTRY_URI and WORKER_REGISTRY_URI are defined in .env in root repo
+export DOCKER_HUB_WEB_REGISTRY_URI=tijcolem/boptest-web
+export DOCKER_HUB_WORKER_REGISTRY_URI=tijcolem/boptest-worker
+
+ if [[ "${GITHUB_REF}" == "refs/heads/boptest-service" ]]; then
+     export VERSION_TAG="develop"
+     echo "The docker tag is set to: ${VERSION_TAG}"
+ elif [[ "${GITHUB_REF}" =~ ^refs/tags/v[0-9].*-service ]]; then
+     export VERSION_TAG="${GITHUB_REF/refs\/tags\//}"
+     echo "The docker tag is set to: ${VERSION_TAG}"
+# use conditional below if you want to build a custom branch
+# elif [[] "${GITHUB_REF}" == "refs/heads/boptest-service-custom" ]]; then 
+#     export VERSION_TAG="experimental"
+ fi
+
+ if [[ "${VERSION_TAG}" == "develop" ]] || [[ "${VERSION_TAG}" =~ ^v[0-9].*-service ]] ; then
+
+    docker tag ${WEB_REGISTRY_URI}:latest ${DOCKER_HUB_WEB_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
+    docker tag ${WORKER_REGISTRY_URI}:latest ${DOCKER_HUB_WORKER_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
+
+    echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+    echo "pushing ${DOCKER_HUB_WEB_REGISTRY_URI}:${VERSION_TAG}"
+    docker push ${DOCKER_HUB_WEB_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
+    echo "pushing ${DOCKER_HUB_WORKER_REGISTRY_URI}:${VERSION_TAG}"
+    docker push ${DOCKER_HUB_WORKER_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
+
+fi
+
+exit $exit_status
