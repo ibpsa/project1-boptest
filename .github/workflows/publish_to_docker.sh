c6e1ab30733a4295ce138f4716264f87950a679b
Tim Coleman <tjc.coleman@gmail.com>
2022-08-26 15:09:55 -0600

    use github action to login to docke

--- .github/workflows/publish_to_docker.sh
+++ .github/workflows/publish_to_docker.sh
@@ -1,4 +1,4 @@
-# load .env defines in root of repo 
+# load .env defines in root of repo
 export $(egrep -v '^#' .env | xargs)
 export DOCKER_HUB_WEB_REGISTRY_URI=tijcolem/boptest-web
 export DOCKER_HUB_WORKER_REGISTRY_URI=tijcolem/boptest-worker
@@ -10,16 +10,20 @@ export DOCKER_HUB_WORKER_REGISTRY_URI=tijcolem/boptest-worker
      export VERSION_TAG="${GITHUB_REF/refs\/tags\//}"
      echo "The docker tag is set to: ${VERSION_TAG}"
 # use conditional below if you want to build a custom branch
-# elif [[] "${GITHUB_REF}" == "refs/heads/boptest-service-custom" ]]; then 
+# elif [[] "${GITHUB_REF}" == "refs/heads/boptest-service-custom" ]]; then
 #     export VERSION_TAG="experimental"
  fi
 
- if [[ "${VERSION_TAG}" == "develop" ]] || [[ "${VERSION_TAG}" =~ ^v[0-9].*-service ]] ; then
+# testing only. remove when done.
+VERSION_TAG="experimental"
+
+ if [[ "${VERSION_TAG}" == "develop" ]] || [[ "${VERSION_TAG}" =~ ^v[0-9].*-service ]] || [[ "${VERSION_TAG}" == "experimental" ]]    ; then
 
     docker tag ${WEB_REGISTRY_URI}:latest ${DOCKER_HUB_WEB_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
     docker tag ${WORKER_REGISTRY_URI}:latest ${DOCKER_HUB_WORKER_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
 
-    echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+    # This should now be done using github action step docker/login-action@f
+    #echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
     echo "pushing ${DOCKER_HUB_WEB_REGISTRY_URI}:${VERSION_TAG}"
     docker push ${DOCKER_HUB_WEB_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
     echo "pushing ${DOCKER_HUB_WORKER_REGISTRY_URI}:${VERSION_TAG}"
