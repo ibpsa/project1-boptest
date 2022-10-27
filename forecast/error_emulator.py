e66ea46ad362a3df931dc2b154fcc08cc144815a
laura.zabala <laura.zabala@r2msolution.com>
2022-10-27 13:24:47 +0200

    Formatting correction in the error_emulator function

--- forecast/error_emulator.py
+++ forecast/error_emulator.py
@@ -1,16 +1,29 @@
+'''
+Created on Sept 5, 2022
+
+@author: Laura Zabala
+
+'''
+
 import numpy as np
 
 def predict_error_AR(hp, F0, K0, F, K, mu):
-    """
-    Generates an error with an AR model in the hp points of the predictions horizon.
-    :parameter hp: int, number of points in the prediction horizon
-    :parameter F0: float, mean of the initial error model
-    :parameter K0: float, standard deviation of the initial error model
-    :parameter F: float from 0 to 1, autocorrelation factor of the AR error model
-    :parameter K: float, standard deviation of the AR error model
-    :parameter mu: float, mean value of the distribution function integrated in the AR error model
-    :return: 1D array with the error values in the hp points
-    """
+    '''Generates an error with an AR model in the hp points of the predictions horizon.
+    
+    Parameters
+    ----------
+    hp : int, number of points in the prediction horizon
+    F0 : float, mean of the initial error model
+    K0 : float, standard deviation of the initial error model
+    F : float from 0 to 1, autocorrelation factor of the AR error model
+    K : float, standard deviation of the AR error model
+    mu : float, mean value of the distribution function integrated in the AR error model
+    
+    Returns
+    -------
+    error : 1D array with the error values in the hp points
+    
+    '''
     
     error = np.zeros(hp)
     error[0,] = np.random.normal(F0, K0)
