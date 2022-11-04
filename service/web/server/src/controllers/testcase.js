eb17cf51829587a7fcdbd98cf343e2245672fccf
Kyle Benne <Kyle.Benne@nrel.gov>
2022-11-03 23:56:27 -0500

    Add my/testcases routes
    ref #9

--- service/web/server/src/controllers/testcase.js
+++ service/web/server/src/controllers/testcase.js
@@ -7,17 +7,21 @@ import {
 } from './test';
 import Path from 'path';
 
-export function getS3KeyForTestcaseID(testcaseid) {
-  return `testcases/shared/${testcaseid}/${testcaseid}.fmu`
+export function getS3KeyForTestcaseID(testcaseid, userid) {
+  let prefix = 'shared'
+  if (userid) {
+    prefix = userid
+  }
+  return `testcases/${prefix}/${testcaseid}/${testcaseid}.fmu`
 }
 
-export function getTestcasePostForm(testcaseid, s3url) {
+export function getTestcasePostForm(testcaseid, s3url, userid) {
   return new Promise((resolve, reject) => {
     // Construct a new postPolicy.
     const params = {
       Bucket: process.env.S3_BUCKET,
       Fields: {
-        key: getS3KeyForTestcaseID(testcaseid)
+        key: getS3KeyForTestcaseID(testcaseid, userid)
       }
     }
     
@@ -34,41 +38,49 @@ export function getTestcasePostForm(testcaseid, s3url) {
   })
 }
 
-export async function getTestcases() {
-  return new Promise((resolve, reject) => {
-    const params = {
-      Bucket: process.env.S3_BUCKET,
-      Prefix: "testcases/shared"
-    }
-
-    s3.listObjectsV2(params, function(err, data) {
-      if (err) {
-        reject(err)
-      } else {
-        const result = data.Contents.map(item => ({ testcaseid: Path.parse(item.Key).name }));
-        resolve(result)
+export async function getTestcases(userid) {
+  const testcasesForPrefix = (prefix) => {
+    return new Promise((resolve, reject) => {
+      const params = {
+        Bucket: process.env.S3_BUCKET,
+        Prefix: prefix
       }
+
+      s3.listObjectsV2(params, function(err, data) {
+        if (err) {
+          reject(err)
+        } else {
+          const result = data.Contents.map(item => ({ testcaseid: Path.parse(item.Key).name }));
+          resolve(result)
+        }
+      })
     })
-  })
+  }
+
+  if (userid) {
+    return await testcasesForPrefix(`testcases/${userid}`)
+  } else {
+    return await testcasesForPrefix('testcases/shared')
+  }
 }
 
-export async function isTestcase(id) {
-  const testcases = await getTestcases()
+export async function isTestcase(id, userid) {
+  const testcases = await getTestcases(userid)
   const found = testcases.find( t => t.testcaseid == id )
   return found != undefined
 }
 
-export async function select(testcaseid, api_key) {
+export async function select(testcaseid, userid) {
   const testid = uuidv4()
-  const key = getS3KeyForTestcaseID(testcaseid)
+  const key = getS3KeyForTestcaseID(testcaseid, userid)
   await setStatus(testid, "Starting")
-  await addJobToQueue("boptest_run_test", {testcaseid, testid, key, api_key})
+  await addJobToQueue("boptest_run_test", {testcaseid, testid, key})
   await waitForStatus(testid, "Running")
   return { testid }
 }
 
-export async function removeTestcase(id) {
-  const key = getS3KeyForTestcaseID(id)
+export async function removeTestcase(id, userid) {
+  const key = getS3KeyForTestcaseID(id, userid)
 
   await new Promise((resolve, reject) => {
     const params = {
--- service/web/server/src/routes/authorized.js
+++ service/web/server/src/routes/authorized.js
@@ -1,12 +1,6 @@
 import express from 'express';
 import got from 'got';
-import {
-  getTestcasePostForm,
-  removeTestcase,
-  isTestcase,
-  getTestcases,
-  select
-} from '../controllers/testcase';
+import * as controller from '../controllers/testcase'
 
 const dashboardServer = process.env.BOPTEST_DASHBOARD_SERVER
 const authorizedRoutes = express.Router()
@@ -15,7 +9,7 @@ const authorizedRoutes = express.Router()
 // This will increase the response time, so use it only on APIs that
 // are not invoked at high frequency. (e.g. avoid using it for APIs
 // such as advance which are typically within a tight simulation loop.)
-export async function authorizer(req, res, next) {
+const authorizer = async (req, res, next)  => {
   const key = req.header('Authorization');
 
   if (key && dashboardServer) {
@@ -31,72 +25,111 @@ export async function authorizer(req, res, next) {
       })
       req.userID = body.sub
     } catch(error) {
-      res.status(401).json({error: 'Not Authorized'})
+      res.sendStatus(401)
       return
     }
   }
   next()
 };
 
-authorizedRoutes.use(authorizer)
+// Middleware to require a valid user
+// This should be used after the authorizer
+const myAuth = (req, res, next) => {
+  if (! req.userID) {
+    res.sendStatus(401)
+    return
+  }
+  next()
+}
 
-authorizedRoutes.get('/testcases/:id/post-form', async (req, res, next) => {
+authorizedRoutes.use(authorizer)
+authorizedRoutes.use('/my/*', myAuth)
+
+const s3url = (req) => {
+  // Goofy logic to consider if s3 is the real amazon service,
+  // or running in a container. Reconsider this nonsense... Review Alfalfa approach.
+  let s3url = null
+  if ( process.env.S3_URL.indexOf("amazonaws") == -1 ) {
+    if (req.hostname.indexOf("web") == -1 ) {
+      s3url = 'http://' + req.hostname + ':9000/' + process.env.S3_BUCKET
+    } else {
+      s3url = 'http://minio:9000/alfalfa'
+    }
+  }
+}
+
+const getTestcasePostForm = async (req, res, next, userID) => {
   try {
     const id = req.params.id
-
-    // Goofy logic to consider if s3 is the real amazon service,
-    // or running in a container. Reconsider this nonsense... Review Alfalfa approach.
-    let s3url = null
-    if ( process.env.S3_URL.indexOf("amazonaws") == -1 ) {
-      if (req.hostname.indexOf("web") == -1 ) {
-        s3url = 'http://' + req.hostname + ':9000/' + process.env.S3_BUCKET
-      } else {
-        s3url = 'http://minio:9000/alfalfa'
-      }
-    }
-
-    const form = await getTestcasePostForm(id, s3url)
-    res.send(JSON.stringify(form))
+    res.json(await controller.getTestcasePostForm(id, s3url(req), userID))
   } catch (e) {
     next(e)
   }
+}
+
+authorizedRoutes.get('/testcases/:id/post-form', async (req, res, next) => {
+  getTestcasePostForm(req, res, next)
 })
 
-authorizedRoutes.delete('/testcases/:id', async (req, res, next) => {
+authorizedRoutes.get('/my/testcases/:id/post-form', async (req, res, next) => {
+  getTestcasePostForm(req, res, next, req.userID)
+})
+
+const removeTestcase = async (req, res, next, userID) => {
   try {
     const id = req.params.id
-    await removeTestcase(id)
+    await controller.removeTestcase(id, userID)
     res.sendStatus(200)
   } catch (e) {
     next(e)
   }
+}
+
+authorizedRoutes.delete('/testcases/:id', async (req, res, next) => {
+  removeTestcase(req, res, next)
 })
 
-authorizedRoutes.post('/testcases/:testcaseid/select', 
-  async (req, res, next) => {
+authorizedRoutes.delete('/my/testcases/:id', async (req, res, next) => {
+  removeTestcase(req, res, next, req.userID)
+})
+
+const select = async (req, res, next, userID) => {
   try {
     const testcaseid = req.params.testcaseid
-    const api_key = req.body['api_key'] || null
-    const response = await select(testcaseid, api_key)
-    return res.json(response)
+    res.json(await controller.select(testcaseid, userID))
   } catch (e) {
     next(e)
   }
+}
+
+authorizedRoutes.post('/testcases/:testcaseid/select', async (req, res, next) => {
+  select(req, res, next)
 });
 
-authorizedRoutes.get('/testcases', async (req, res, next) => {
+authorizedRoutes.post('/my/testcases/:testcaseid/select', async (req, res, next) => {
+  select(req, res, next, req.userID)
+});
+
+const getTestcases = async (req, res, next, userID) => {
   try {
-    const payload = await getTestcases()
-    res.json(payload)
+    res.json(await controller.getTestcases(userID))
   } catch (e) {
     next(e);
   }
+}
+
+authorizedRoutes.get('/testcases', async (req, res, next) => {
+  getTestcases(req, res, next)
 })
 
-authorizedRoutes.get('/testcases/:testcaseid', async (req, res, next) => {
+authorizedRoutes.get('/my/testcases', async (req, res, next) => {
+  getTestcases(req, res, next, req.userID)
+})
+
+const isTestcase = async (req, res, next, userID) => {
   try {
     const testcaseid = req.params.testcaseid
-    if (await isTestcase(testcaseid)) {
+    if (await controller.isTestcase(testcaseid, userID)) {
       res.sendStatus(200)
     } else {
       res.sendStatus(404)
@@ -104,6 +137,14 @@ authorizedRoutes.get('/testcases/:testcaseid', async (req, res, next) => {
   } catch (e) {
     next(e);
   }
+}
+
+authorizedRoutes.get('/testcases/:testcaseid', async (req, res, next) => {
+  isTestcase(req, res, next)
+})
+
+authorizedRoutes.get('/my/testcases/:testcaseid', async (req, res, next) => {
+  isTestcase(req, res, next, req.userID)
 })
 
 export default authorizedRoutes;
