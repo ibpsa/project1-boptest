fbed98d543f8b324e6475755ae64ae3ba61e9fa7
Kyle Benne <Kyle.Benne@nrel.gov>
2022-12-14 12:34:20 -0600

    Report boptest-service version from `/version` endpoint

--- service/web/server/src/controllers/utility.js
+++ service/web/server/src/controllers/utility.js
@@ -1,19 +1,11 @@
-import fs from 'fs';
+import { promises as fs } from "fs";
 
 export async function getVersion() {
-  return new Promise((resolve, reject) => {
-    fs.readFile('/boptest/version.txt', 'utf8', function (error, data) {
-      if (error) {
-        const status = 500;
-        const message = "BOPTEST version is unknown."
-        const payload = {'version': 'null'};
-        resolve({status, message, payload});
-      } else {
-        const status = 200;
-        const message = "Queried the version number successfully."
-        const payload = {'version': data};
-        resolve({status, message, payload});
-      }
-    })
-  })
+  const libraryVersion = (await fs.readFile('/boptest/version.txt', 'utf8')).trim()
+  const serviceVersion = (await fs.readFile('/boptest/service-version.txt', 'utf8')).trim()
+  
+  const status = 200;
+  const message = "Queried the version number successfully."
+  const payload = {'version': libraryVersion, 'service-version': serviceVersion}
+  return {status, message, payload}
 }
