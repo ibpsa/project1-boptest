851a3bdd015bfde72e0b7254185ce781ab7c4c3b
Tim Coleman <tjc.coleman@gmail.com>
2023-03-31 11:39:20 -0600

    fix the envs in the docker push as these are no longer in .env

--- service/ci/publish_to_docker.sh
+++ service/ci/publish_to_docker.sh
@@ -1,5 +1,9 @@
 # load .env defines in root of repo
 export $(egrep -v '^#' .env | xargs)
+export WEB_REGISTRY_URI=boptest-service-web
+export WORKER_REGISTRY_URI=boptest-service-worker
+
+
 export DOCKER_HUB_WEB_REGISTRY_URI=nrel/boptest-web
 export DOCKER_HUB_WORKER_REGISTRY_URI=nrel/boptest-worker
 
