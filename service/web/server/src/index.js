87577d4f10817aa2ef544e5faf6cf7f980294144
Kyle Benne <Kyle.Benne@nrel.gov>
2023-09-12 16:30:29 -0500

    Custom json parsing for Infinity

--- service/web/server/src/index.js
+++ service/web/server/src/index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import boptestRoutes from './routes/boptestRoutes'
 import { errorHandler } from './routes/error'
 import { parse } from 'url'
@@ -7,8 +6,27 @@ import { createServer } from 'http'
 import { createBoptestWS } from './ws/boptestWS'
 
 const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+
+// app.use(express.json())
+// It would be best to use express.json, however for now we need to support
+// non standard keywords (Infinity / -Infinity) in the json body.
+//
+// express.raw extracts the body without json parsing
+app.use(express.raw({ type: 'application/json' }))
+// Then use custom json parsing to handle Infinity
+// The solution here is to just quote the Infinity keyword,
+// allowing the string to pass through to the boptest library
+app.use((req, res, next) => {
+  if (req.is('application/json')) {
+    let body = req.body.toString()
+    body = body.replaceAll(' -Infinity', ' "-Infinity"')
+    body = body.replaceAll(' Infinity', ' "Infinity"')
+    req.body = JSON.parse(body)
+  }
+  next()
+})
+
+app.use(express.urlencoded({ extended: true }))
 app.use('/', boptestRoutes)
 app.use(errorHandler)
 
