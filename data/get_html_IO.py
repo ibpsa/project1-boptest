e3316d7badac41d7b6df51fb9de27e582b9c6162
ettore zanetti <ettore.zanetti@polimi.it>
2024-02-21 20:12:21 -0800

    Address review comments

--- data/get_html_IO.py
+++ data/get_html_IO.py
@@ -11,7 +11,7 @@ Outputs:
 "inputs.html": html code documenting the inputs
 "measurements.html": html code documenting the outputs
 "forecast_points.html" html code documenting the forecasts
-"inputs_measurement_forecasts.html" html code documenting inputs,outputs and 
+"inputs_measurements_forecasts.html" html code documenting inputs,outputs and 
 forecasts together
 """
 
@@ -41,28 +41,6 @@ def run():
     # ---------------
 
     # GET TEST INFORMATION
-    # --------------------
-    # Inputs available
-    inputs = requests.get('{0}/inputs'.format(url)).json()['payload']
-    with open('inputs.html', 'w') as f:
-        for i in sorted(inputs.keys()):
-            if 'activate' not in i:
-                f.write('<li>\n<code>{0}</code> [{1}] [min={2}, max={3}]: {4}\n</li>\n'.format(i,inputs[i]['Unit'],inputs[i]['Minimum'], inputs[i]['Maximum'], inputs[i]['Description']))
-            else:
-                f.write('<li>\n<code>{0}</code> [1] [min=0, max=1]: Activation signal to overwrite input {1} where 1 activates, 0 deactivates (default value)\n</li>\n'.format(i,i.replace('activate','')+'u'))
-
-    # Measurements available
-    measurements = requests.get('{0}/measurements'.format(url)).json()['payload']
-    with open('measurements.html', 'w') as f:
-        for i in sorted(measurements.keys()):
-            if 'activate' not in i:
-                f.write('<li>\n<code>{0}</code> [{1}] [min={2}, max={3}]: {4}\n</li>\n'.format(i,measurements[i]['Unit'],measurements[i]['Minimum'], measurements[i]['Maximum'], measurements[i]['Description']))
-    # Forecasts available
-    forecast_points = requests.get('{0}/forecast_points'.format(url)).json()['payload']
-    with open('forecast_points.html', 'w') as f:
-        for i in sorted(forecast_points.keys()):
-            if 'activate' not in i:
-                f.write('<li>\n<code>{0}</code> [{1}]: {2}\n</li>\n'.format(i,forecast_points[i]['Unit'],forecast_points[i]['Description']))
     # --------------------
     # Create single I/O file
     # Inputs available
