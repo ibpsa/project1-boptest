9b64b50f0c06eb0f569494c31bde3d499e4fc1bf
David Blum <dhblum@lbl.gov>
2024-08-06 10:14:51 -0700

    Update find_days and days.json

--- testcases/multizone_office_simple_hydronic/models/find_days.py
+++ testcases/multizone_office_simple_hydronic/models/find_days.py
@@ -8,10 +8,12 @@ This script generates the days for the different scenarios.
 from data.find_days import find_days
 import json
 
-days = find_days(heat='heat',
-                 cool='cold',
-                 data='sim_heat_cold.csv',
-                 img_name=None)
+days = find_days(heat='heating_cooling.heapro.Q_flow',
+                 cool='heating_cooling.coopro.Q_flow',
+                 data='HeatingCooling.csv',
+                 cooling_negative=True,
+                 cool_day_low_limit=77+7,
+                 plot=True)
 
 with open('days.json', 'w') as f:
     json.dump(days, f)
