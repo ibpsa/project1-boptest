c3adc85965f7b1510d556fd4718afc39f0dd8400
David Blum <dhblum@lbl.gov>
2024-08-24 10:18:48 -0700

    Add tests for new find_days functionality

--- testcases/multizone_office_simple_hydronic/models/find_days.py
+++ testcases/multizone_office_simple_hydronic/models/find_days.py
@@ -12,7 +12,7 @@ days = find_days(heat='heating_cooling.heapro.Q_flow',
                  cool='heating_cooling.coopro.Q_flow',
                  data='HeatingCooling.csv',
                  cooling_negative=True,
-                 cool_day_low_limit=77+7,
+                 cool_day_low_limit=77+7, # Without limit, typical day is the first day cooling turns on
                  plot=True)
 
 with open('days.json', 'w') as f:
