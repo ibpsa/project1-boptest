b648373ae72a81789c5e8aa25a59ad579a95ae5d
Kyle Benne <Kyle.Benne@nrel.gov>
2024-10-10 11:13:20 -0500

    Remove "service-version"
    It no longer makes sense to maintain a unique version of the service.
    There is now simply the BOPTEST version.

--- service/web/server/src/lib/boptestLib.js
+++ service/web/server/src/lib/boptestLib.js
@@ -44,11 +44,10 @@ function getUserTestsKey(userSub) {
 
 export async function getVersion() {
   const libraryVersion = (await fs.readFile('/boptest/version.txt', 'utf8')).trim()
-  const serviceVersion = (await fs.readFile('/boptest/service-version.txt', 'utf8')).trim()
 
   const status = 200;
   const message = "Queried the version number successfully."
-  const payload = { 'version': libraryVersion, 'service-version': serviceVersion }
+  const payload = { 'version': libraryVersion }
   return { status, message, payload }
 }
 
