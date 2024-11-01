c9014b958b114cebac0b4b7001986f03e6147f63
terrancelu92 <40041133+terrancelu92@users.noreply.github.com>
2024-10-31 17:06:24 -0700

    Revert the debugging environment files

--- run_docker_parser.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Step 1: Build the Docker image
-docker build -f Dockerfile_JModelica -t jmodelica_debug_image .
-
-REM Step 2: Run the Docker container without --rm to keep it alive
-docker run -d -p 5679:5679 -v C:\Github\project1-boptest\parsing:/workspace -v C:\Github\project1-boptest\data:/workspace/data --name jmodelica_debug jmodelica_debug_image tail -f /dev/null
-
-REM Step 3: Checkout to workspace and run the script inside the container
-docker exec -it jmodelica_debug bash -c "cd /workspace && python parser.py"
