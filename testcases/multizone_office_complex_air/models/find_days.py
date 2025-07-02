92e52499ec6a76a90dfed63dca5cc3a25a648c74
David Blum <dhblum@lbl.gov>
2025-07-01 19:13:16 -0700

    Update find_days.py

--- testcases/multizone_office_complex_air/models/find_days.py
+++ testcases/multizone_office_complex_air/models/find_days.py
@@ -7,11 +7,10 @@ This case needs to be deployed if using BOPTEST image.
 from data.find_days import find_days
 import json
 
-days = find_days(heat='hvac.boiWatPla.QTot.y',
-                 cool='hvac.chiWatPla.QTot.y',
+days = find_days(heat='hva.boiWatPla.QTot.y',
+                 cool='hva.chiWatPla.QTot.y',
                  data='sim_heat_cool.csv',
-                 peak_cool_restriction_hour=9,
-                 img_name=None)
+                 peak_cool_restriction_hour=9)
 
 with open('days.json', 'w') as f:
     json.dump(days, f)
