1bbfbe6f24f2d31e857655f2aaaf0d9b6058aa58
ettore zanetti <ettore.zanetti@polimi.it>
2026-05-08 13:35:22 -0700

    Update test cases scenario days and fix multizone_simple_office_air effectiveness calculation

--- testcases/multizone_residential_hydronic/models/find_days.py
+++ testcases/multizone_residential_hydronic/models/find_days.py
@@ -7,8 +7,7 @@ This case needs to be deployed if using BOPTEST image.
 from data.find_days import find_days
 import json
 
-days = find_days(heat='sum_Q_flow', cool=None,
-                 data='TestCase.csv')
+days = find_days(heat='boi.QWat_flow.y', cool=None)
 
 with open('days.json', 'w') as f:
     json.dump(days, f)
